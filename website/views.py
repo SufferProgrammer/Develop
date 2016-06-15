72fcda99119d08b1bee0a20a03d557951b2a05ce
SufferProgrammer <pijipirma@gmail.com>
2016-06-15 07:38:47 +0700

    Revision 6

--- website/views.py
+++ website/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils import timezone
+from models import Post
 
 def index(request):
-	return render(request, "website/index.htm", {})
+	post = Post.objects.filter(Created__lte=timezone.now()).order_by('Created')
+	return render(request, 'website/index.htm', {'post' : post})
